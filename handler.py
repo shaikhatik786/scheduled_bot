91de5e4af5fd545ab5ceeed5352cadd8d2c91378
shaikhatik786 <48386704+shaikhatik786@users.noreply.github.com>
2020-03-19 20:51:31 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import telegram
 import sys
 import os
-TOKEN = os.environ[‘TELEGRAM_TOKEN’]
+TOKEN = os.environ[‘992176702:AAE62G_ubiBo1tGq8x0sQ2W2yYVtfRHBHxk’]
 CHAT_ID = -1001168759783
 def send_message(event, context):
     bot = telegram.Bot(token=TOKEN)
